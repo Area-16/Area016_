65ade3f86f6be2ec07bcdc6f37226ad01efedbed
Douglas Alves <douglas.expalves@gmail.com>
2018-04-04 15:00:01 -0300

    add server setup

---
+++ src/server/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import compression from 'compression'
+import responseTime from 'response-time'
+import helmet from 'helmet'
+
+import { getEnv } from './../config/env'
+import cors from './cors'
+import mailRoute from '../api/controllers/mail-controller'
+
+const app = express()
+const envValues = getEnv()
+
+app.use([json(), urlencoded({ extended: true })])
+app.use([compression(), responseTime()])
+app.use(helmet({ hidePoweredBy: true }))
+app.use(cors)
+app.use('/api', [mailRoute])
+app.set('port', parseInt(envValues.PORT) || 3000)
+
+function Start() { 
+	app.listen(app.get('port'), '0.0.0.0', (err) => {
+		if (err) {
+			console.error(err)
+			process.exit(1)
+		}
+		console.info('Server running on port:', app.get('port'))
+	})
+}
+
+export { Start }
+export default app
